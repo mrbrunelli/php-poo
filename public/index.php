833b27001fae745f84b4c095cd7627df43410438
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2020-09-10 19:22:51 -0300

    refactor the account

--- public/index.php
+++ public/index.php
@@ -7,16 +7,11 @@ require_once '../vendor/autoload.php';
 use Alfa\Conta;
 use Alfa\Util\Date;
 
-$minhaConta = new Conta();
-$minhaConta->saldo = 500.00;
-$minhaConta->titular = 'João';
-
-$minhaContaDosSonhos = new Conta();
-$minhaContaDosSonhos->saldo = 1_000_000_000.00;
-$minhaContaDosSonhos->titular = 'José';
-
+$minhaConta = new Conta(1, 'João', 500.00);
+dump($minhaConta->sacar(600.00));
+$minhaContaDosSonhos = new Conta(2, 'José', 1_000_000_000.00);
 $minhaContaDosSonhos->tranferir($minhaConta, 500_000.00);
-
+echo $minhaConta->pegaSaldo();
 dump($minhaConta);
 dump($minhaContaDosSonhos);
 dump(Conta::$movimentacoes);
--- src/Conta.php
+++ src/Conta.php
@@ -6,28 +6,39 @@ namespace Alfa;
 
 class Conta
 {
-  public int $numero;
-  public float $saldo;
-  public string $titular;
+  private int $numero;
+  private string $titular;
+  private float $saldo;
+  private float $taxa = 5.00;
+  private float $limite = 1000.00;
   public static array $movimentacoes = [];
+
+  public function __construct(int $numero, string $titular, float $saldo)
+  {
+    $this->numero = $numero;
+    $this->titular = $titular;
+    $this->saldo = $saldo;
+  }
 
   public function sacar(float $valor): bool
   {
-    if ($this->saldo < $valor) {
+    if (!$this->possuiSaldo($valor)) {
       return false;
     }
-
     $this->saldo -= $valor;
     self::$movimentacoes[] = sprintf("[%s] Saque %s", $this->titular, $valor);
-
     return true;
+  }
+
+  private function possuiSaldo(float $valor): bool
+  {
+    return ($this->pegaSaldo() >= $valor);
   }
 
   public function depositar(float $valor): void
   {
     $this->saldo += $valor;
     self::$movimentacoes[] = sprintf("[%s] Deposito %s", $this->titular, $valor);
-
   }
 
   public function tranferir(Conta $contaDestino, float $valor): bool
@@ -36,7 +47,11 @@ class Conta
     if ($retirou) {
       $contaDestino->depositar($valor);
     }
-
     return $retirou;
+  }
+
+  public function pegaSaldo(): float
+  {
+    return $this->saldo - $this->taxa + $this->limite;
   }
 }
